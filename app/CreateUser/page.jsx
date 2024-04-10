582a40fc7cf48e8f0021d613015fcc2ae87fe200
Rubem Staciarini Puttini <rstp@vtxrm.com>
2024-04-10 18:03:25 +0100

    finish mvp

--- app/CreateUser/page.jsx
+++ app/CreateUser/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import UserForm from '../(components)/UserForm'
 
 const CreateUser = () => {
     return (
-        <div>Only Admins!</div>
+        <div><UserForm /></div>
     )
 }
 
--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,11 +1,14 @@
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/app/(models)/User";
+import bcrypt from "bcrypt";
 
 export const options = {
     providers: [
         GitHubProvider({
             profile(profile) {
-                console.log("Profile GitHub: ", profile)
+                console.log("Profile GitHub: ", profile);
 
                 let userRole = "GitHub User";
 
@@ -24,7 +27,9 @@ export const options = {
 
         GoogleProvider({
             profile(profile) {
-                console.log("Profile Google: ", profile)
+                console.log("Profile Google: ", profile);
+
+                let userRole = "Google User";
 
                 return {
                     ...profile,
@@ -34,6 +39,51 @@ export const options = {
             },
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
+        }),
+
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "email:",
+                    type: "text",
+                    placeholder: "your-email",
+                },
+                password: {
+                    label: "password:",
+                    type: "password",
+                    placeholder: "your-password",
+                },
+            },
+            async authorize(credentials) {
+                try {
+                    const foundUser = await User.findOne({email: credentials.email})
+                        .lean()
+                        .exec();
+
+                    if (foundUser) {
+                        console.log("User Exists");
+
+                        const match = await bcrypt.compare(
+                            credentials.password,
+                            foundUser.password
+                        );
+
+                        if (match) {
+                            console.log("Good Pass");
+                            delete foundUser.password;
+
+                            foundUser["role"] = "Unverified Email";
+                            return foundUser;
+                        }
+                    }    
+                    
+                } catch (error) {
+                    console.log(error);
+
+                }
+                return null;
+            }
         }),
     ],
 
